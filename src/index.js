96af62748b3359a9f3072ca07aa40d87fe35464a
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-09-24 00:06:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://ayush:ayush123@cluster0.swqiskf.mongodb.net/firts_db?retryWrites=true&w=majority"
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
